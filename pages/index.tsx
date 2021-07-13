4035d829d231b654e5cc60f30d4f9fa00b91816c
jongsik <kjsik11@gmail.com>
2021-07-13 11:29:38 +0900

    add code block test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,125 +3,29 @@
  */
 
 import React, { useState } from 'react';
-import NextLink from 'next/link';
-
-import { useUI } from '@components/context';
-import { Button } from '@components/ui';
-import { fetcher } from '@lib/fetcher';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 export default function IndexPage() {
-  const { showModal, closeModal, showNoti } = useUI();
-  const [result, setResult] = useState<string | null>(null);
-
+  const codeString = `
+  Example 2: Generate random number between two numbers
+  // generating random number in range [x, y)
+  function getRandomNum(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  // random number in range 5(inclusive) and 10(exclusive)
+  var random_num = getRandomNum(5, 10);
+  console.log(random_num);
+  
+  // random number in range 0(inclusive) and 100(exclusive)
+  var random_num = getRandomNum(0, 100);
+  console.log(random_num);`;
   return (
-    <div className="mx-auto max-w-screen-lg text-2xl pt-4 h-[1200px] flex justify-center">
-      {/* <p className="text-xl">hello world</p> */}
-      <div className="space-y-4 w-full">
-        <div
-          className="w-20 h-20 bg-black sm:bg-red-500 md:bg-yellow-500 lg:bg-blue-500 xl:bg-green-500 2xl:bg-sky-500"
-          aria-hidden="true"
-        />
-        <div
-          className="w-20 h-20 bg-black/40 sm:bg-red-500/40 md:bg-yellow-500/40 lg:bg-blue-500/40 xl:bg-green-500/40 2xl:bg-sky-500/40"
-          aria-hidden="true"
-        />
-        <div className="w-20 h-20 bg-gradient-to-br from-black/40 to-white/60" aria-hidden="true" />
-        <div>
-          <Button
-            color="red"
-            onClick={() =>
-              showModal({
-                variant: 'alert',
-                title: '댓글을 완전히 삭제할까요?',
-                content:
-                  '삭제된 댓글은 복구할 수 없습니다. 댓글에 달린 모든 답글도 함께 삭제됩니다.',
-                actionButton: {
-                  label: '삭제',
-                  onClick: () => {
-                    closeModal();
-                  },
-                },
-                cancelButton: {
-                  label: '취소',
-                  onClick: () => closeModal(),
-                },
-              })
-            }
-          >
-            open alert modal
-          </Button>
-        </div>
-
-        <div>
-          <Button
-            onClick={() =>
-              showModal({
-                title: '댓글을 완전히 복구?',
-                content:
-                  '복구된 댓글은 삭제할 수 없습니다. 댓글에 달린 모든 답글도 함께 복구됩니다.',
-                actionButton: {
-                  label: '복구',
-                  onClick: () => {
-                    closeModal();
-                  },
-                },
-                cancelButton: {
-                  label: '취소',
-                  onClick: () => closeModal(),
-                },
-              })
-            }
-          >
-            open modal
-          </Button>
-        </div>
-        <div>
-          <Button
-            color="white"
-            onClick={() => showNoti({ title: '알람입니다.', variant: 'alert' })}
-          >
-            alert Noti
-          </Button>
-        </div>
-        <div className="space-x-4">
-          <NextLink href="/test" passHref>
-            <Button as="a">Button as Anchor</Button>
-          </NextLink>
-          <NextLink href="/test">
-            <Button>push button</Button>
-          </NextLink>
-        </div>
-        <div className="space-x-4">
-          <Button
-            onClick={() => {
-              fetcher('/api')
-                .then((res) => setResult(JSON.stringify(res)))
-                .catch((err) => showNoti({ variant: 'alert', title: err.message }));
-            }}
-          >
-            STATUS
-          </Button>
-          <Button
-            color="red"
-            onClick={() => {
-              fetcher('/api/error')
-                .then((res) => setResult(JSON.stringify(res)))
-                .catch((err) => showNoti({ variant: 'alert', title: err.message }));
-            }}
-          >
-            ERROR
-          </Button>
-          <Button
-            color="white"
-            onClick={() => {
-              setResult(null);
-            }}
-          >
-            CLEAR
-          </Button>
-        </div>
-        <p className="text-lg">{result || 'null'}</p>
-      </div>
+    <div>
+      <SyntaxHighlighter language="python" style={docco} error={false}>
+        {codeString}
+      </SyntaxHighlighter>
     </div>
   );
 }
